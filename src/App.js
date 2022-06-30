3caef7411e1044de4dbd47ea79fbcdc1aaaef3e1
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-07-01 00:21:14 +0530

    Added sliding effect with smooth transition

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
 import Slider from "./components/Slider";
-import { AiFillCaretRight } from "react-icons/ai";
-import { AiFillCaretLeft } from "react-icons/ai";
-import { FiPause } from "react-icons/fi";
-import { useEffect, useState } from "react";
 import Data from "./components/data.json";
 
 function App() {
-    const projects = Data;
-    const [slide, setSlide] = useState(0);
-
-    function changeSlide(action) {
-        if (action === "prev") {
-            if (slide !== 0) {
-                setSlide(slide - 1);
-            } else {
-                setSlide(projects.length - 1);
-            }
-        } else {
-            if (slide === projects.length - 1) {
-                setSlide(0);
-            } else {
-                setSlide(slide + 1);
-            }
-        }
-    }
-
-    return (
-        <div className="App">
-            <div className="slider">
-                <Slider key={projects[slide].key} {...projects[slide]} />
-            </div>
-            <div className="buttons">
-                <button onClick={() => changeSlide("prev")}>
-                    <AiFillCaretLeft />
-                </button>
-                <button>
-                    <FiPause />
-                </button>
-                <button onClick={() => changeSlide("next")}>
-                    <AiFillCaretRight />
-                </button>
-            </div>
-        </div>
-    );
+    return <Slider data={Data} />;
 }
 
 export default App;
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { HiOutlineCode } from "react-icons/hi";
 import { BiLinkAlt } from "react-icons/bi";
+import { AiFillCaretRight } from "react-icons/ai";
+import { AiFillCaretLeft } from "react-icons/ai";
+import { FiPause } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import "./slider.css";
 
-function Slider({ key, title, description, live_url, code_url }) {
+function Slide({ cname, id, title, description, live_url, code_url }) {
     return (
-        <div className="show" id={key}>
+        <div className={cname} id={id}>
             <h4>{title}</h4>
             <small className="desc">{description}</small>
             <div className="links-wrapper">
-                <a href={live_url} target="_blank" rel="noreferrer">
+                <a href={code_url} target="_blank" rel="noreferrer">
                     Code
                     <HiOutlineCode className="a-in"></HiOutlineCode>
                 </a>
-                <a href={code_url} target="_blank" rel="noreferrer">
+                <a href={live_url} target="_blank" rel="noreferrer">
                     Visit
                     <BiLinkAlt className="a-in" />
                 </a>
@@ -21,4 +26,50 @@ function Slider({ key, title, description, live_url, code_url }) {
     );
 }
 
-export default Slider;
+export default function Slider(props) {
+    const projects = props.data;
+    const [slide, setSlide] = useState(0);
+
+    useEffect(() => {
+        const last = projects.length - 1;
+        if (slide < 0) {
+            setSlide(last);
+        } else if (slide > last) {
+            setSlide(0);
+        }
+    }, [slide, projects]);
+
+    return (
+        <div className="component">
+            <div className="slider">
+                {projects.map((project, index) => {
+                    let position = "show";
+                    if (slide === index) {
+                        position = "show active";
+                    } else if (
+                        index === slide - 1 ||
+                        (slide === 0 && index === projects.length - 1)
+                    ) {
+                        position = "show next";
+                    } else {
+                        position = "show prev";
+                    }
+                    return (
+                        <Slide key={project.id} cname={position} {...project} />
+                    );
+                })}
+            </div>
+            <div className="buttons">
+                <button onClick={() => setSlide(slide - 1)}>
+                    <AiFillCaretLeft />
+                </button>
+                <button>
+                    <FiPause />
+                </button>
+                <button onClick={() => setSlide(slide + 1)}>
+                    <AiFillCaretRight />
+                </button>
+            </div>
+        </div>
+    );
+}
